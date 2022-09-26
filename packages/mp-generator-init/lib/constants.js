d4f3f06ab8d600fc69cede4cfb0d7a3814bb75d5
Zhang Lei <372470745@qq.com>
2022-09-26 18:54:15 +0800

    add: devDependencies

---
+++ packages/mp-generator-init/lib/constants.js
@@ -0,0 +1,11 @@
+// 缓存模板的目录名称
+const TEMPLATE_CACH_DIRNAME = ".mp-generator-templates";
+
+// 模板组仓库地址
+const TEMPLATE_REPO_PATH =
+  "https://github.com/PassionZale/mp-generator-templates.git";
+
+module.exports = {
+  TEMPLATE_CACH_DIRNAME,
+  TEMPLATE_REPO_PATH,
+};
--- packages/mp-generator-init/lib/init_command.js
+++ packages/mp-generator-init/lib/init_command.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 function initCommand() {
     // TODO
 }
