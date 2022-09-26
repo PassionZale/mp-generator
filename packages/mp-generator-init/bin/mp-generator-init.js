e31fd89149ca0affbe1d6929eecd2cd0b723f104
Zhang Lei <372470745@qq.com>
2022-09-26 18:57:53 +0800

    config: 增加 main 入口

--- packages/mp-generator-init/bin/mp-generator-init.js
+++ packages/mp-generator-init/bin/mp-generator-init.js
@@ -4,4 +4,6 @@
 
 console.log('mp-generator-init-enty-point')
 
-module.exports = {}
+const Command = require("..")
+
+Command()
--- packages/mp-generator-init/lib/init_command.js
+++ packages/mp-generator-init/lib/init_command.js
@@ -1,7 +1,7 @@
-'use strict';
+"use strict";
 
 function initCommand() {
-    // TODO
+  console.log("initCommand");
 }
 
 module.exports = initCommand;
